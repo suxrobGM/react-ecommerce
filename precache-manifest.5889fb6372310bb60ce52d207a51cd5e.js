676ab8002515e35d455e0770f4459e11f6eb5fd5
SuxrobGM <suxrobGM@gmail.com>
2019-08-18 13:06:09 +0500

    Updates

---
+++ precache-manifest.5889fb6372310bb60ce52d207a51cd5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "965f381223300054308a",
+    "url": "/ReactEcommerce/static/js/runtime~main.965f3812.js"
+  },
+  {
+    "revision": "70253f1439e64fd98990",
+    "url": "/ReactEcommerce/static/js/main.70253f14.chunk.js"
+  },
+  {
+    "revision": "09c37d1714a8c553bbe2",
+    "url": "/ReactEcommerce/static/js/2.09c37d17.chunk.js"
+  },
+  {
+    "revision": "70253f1439e64fd98990",
+    "url": "/ReactEcommerce/static/css/main.f3c99c1e.chunk.css"
+  },
+  {
+    "revision": "01c539e9c04c7e2bd759d5296a5a3f57",
+    "url": "/ReactEcommerce/index.html"
+  }
+];
